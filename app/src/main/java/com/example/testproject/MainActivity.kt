84d08297c7d66705228b01a3bc0428772fe2781f
amirfahed777@gmail.com <amirfahed777@gmail.com>
2021-10-09 22:14:25 +0300

    lesson 4 end

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.testproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.textView)
+    }
+
+    fun toastMe(view: View){
+        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+
+    fun countMe(view: View){
+        var countString = textView.text.toString()
+        countString = (countString.toInt() + 1).toString()
+        textView.text = countString
     }
 }
